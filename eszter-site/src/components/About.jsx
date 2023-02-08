d5082804aca12340d8ea3d3a624213153921fdd4
DanGraham23 <d20grah@siena.edu>
2023-02-08 18:31:10 -0500

    add starter about info and photo

--- eszter-site/src/components/About.jsx
+++ eszter-site/src/components/About.jsx
@@ -1,8 +1,10 @@
 export default function About (){
     return (
         <div className="about-container" id="about">
-            <p>Eszter Kiss is a college student at SUNY New Paltz. Her major is Physical Education. Her goal in life is to become an educator in the field of physical education. She runs a program for female athletes, teaching all them about issues that come up in women’s sports. She also coaches at a sports camp and she does a lot of advocacy work for all students on the New Paltz campus. In her free time she likes to participate in a variety of sports, one of being distance triathlons. In 2022 she finished her first Ironman triathlon in Lake Placid. </p>
-            <img src="/assets/eszter-photo.jpg" alt="Eszter-photo" className="eszter-photo"/>
+            <div className="about-content">
+                <p className="bio">Eszter Kiss is a college student at SUNY New Paltz. Her major is Physical Education. Her goal in life is to become an educator in the field of physical education. She runs a program for female athletes, teaching all them about issues that come up in women’s sports. She also coaches at a sports camp and she does a lot of advocacy work for all students on the New Paltz campus. In her free time she likes to participate in a variety of sports, one of being distance triathlons. In 2022 she finished her first Ironman triathlon in Lake Placid. </p>
+                <img src="/assets/eszter-photo.jpg" alt="Eszter-photo" className="eszter-photo"/>
+            </div>
         </div>
     )
 }

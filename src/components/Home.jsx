75fad15a36768cd482ed63f6a17accdf90ddcc04
JonathanMasih <j24masi@siena.edu>
2023-03-05 17:55:41 -0500

    reponsiveness added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
-export default function Home(){
+export default function Home() {
     return (
         <div className="home-container" id="home">
-            
+            <img className="banner-image" src='../../../assets/home-banner.jpg' alt="home-banner" />
         </div>
     )
 }
